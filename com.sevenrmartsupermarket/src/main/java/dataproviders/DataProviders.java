454b67f1eab5bc79405105c32a684ebbe0441c85
reema <reema@Reema>
2024-03-15 19:12:19 +0400

    DataProvider commit

---
+++ com.sevenrmartsupermarket/src/main/java/dataproviders/DataProviders.java
@@ -0,0 +1,25 @@
+package dataproviders;
+
+import org.testng.annotations.DataProvider;
+
+import com.sevenrmartsupermarket.utilities.Excel;
+
+public class DataProviders {
+	
+	Excel excel= new Excel();
+
+
+	@DataProvider(name="sevenmartsupermarket")
+	public Object[][] login()
+	{
+		return new Object [][] {{"Reema","Man"},{"Gigo","Jijo"},{"Jesica","Manu"}};
+	}
+	
+	@DataProvider(name="multilogin")
+	public Object[][]adminlogin(){
+	
+		excel.setExcelFile("LoginData", "adminuser");
+		return excel.getMultidimentionalData(3, 2);
+	
+	}
+}
---
+++ com.sevenrmartsupermarket/src/test/java/com/sevenrmartsupermarket/tests/LoginwithDataproviders.java
@@ -0,0 +1,28 @@
+package com.sevenrmartsupermarket.tests;
+
+import org.testng.annotations.Test;
+
+import com.sevenrmartsupermarket.base.Base;
+import com.sevenrmartsupermarket.pages.LoginPage;
+
+import dataproviders.DataProviders;
+
+public class LoginwithDataproviders extends Base {
+	
+	LoginPage loginpage;
+	
+	
+	@Test(dataProvider ="multilogin", dataProviderClass = DataProviders.class )
+	
+	public void verifyLoginWithDifferntAdminUsers(String userName, String Password) {
+		loginpage = new LoginPage(driver);
+		
+		loginpage.login(userName, Password);
+		
+		
+		
+	}	
+	
+	
+
+}
